b7cec4991f37c7e98bd088b289143e2667afbd32
Hongyu Yan <133737661+Markyan04@users.noreply.github.com>
2024-03-25 16:26:45 +0800

    TOKEN有效期变更

--- application/config/config.py
+++ application/config/config.py
@@ -4,7 +4,7 @@
 
 
 class Settings:
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     SECRET_KEY: str = "f2e1f1b1c1a1"
     ALGORITHMS: str = "HS256"
     IMG_PATH: str = "frontend/public/tongue"
