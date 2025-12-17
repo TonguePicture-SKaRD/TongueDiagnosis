676a9863da951875f99342a2391701b38ac317e2
Hongyu Yan <133737661+Markyan04@users.noreply.github.com>
2025-12-17 21:15:38 +0800

    update the package file

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,6 @@
 export const settings = {
-    ServerUrl: 'http://192.168.1.133:5000',
+    // ServerUrl: 'http://192.168.1.133:5000',
+    ServerUrl: 'http://10.252.133.135:5000'
 };
 
 export default settings;
